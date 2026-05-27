748e7348419232077a57d032b3d65c857fabc1f9
QualityOwl <josh@qualityowl.llc>
2026-05-27 16:35:00 -0500

    Remove hardcoded credentials

--- performance/config/environments.js
+++ performance/config/environments.js
@@ -1,3 +1,3 @@
 export const BASE_URL = __ENV.BASE_URL || 'https://restful-booker.herokuapp.com';
-export const API_USERNAME = __ENV.API_USERNAME || 'admin';
-export const API_PASSWORD = __ENV.API_PASSWORD || 'password123';
+export const API_USERNAME = __ENV.API_USERNAME;
+export const API_PASSWORD = __ENV.API_PASSWORD;
